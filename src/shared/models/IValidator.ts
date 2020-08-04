4723fa10f122e7ab13d8cfe00553f27f14d5a63e
Pedro Lages <pxd3v@users.noreply.github.com>
2020-08-04 18:38:21 -0300

    [enhancement]

--- src/shared/models/IValidator.ts
+++
@@ -1,5 +0,0 @@
-export default interface IValidator<Dto> {
-    execute: (data: Dto) => Promise<void>;
-    validate: (data: Dto) => Promise<void>;
-    validateFormat: (data: Dto) => Promise<void>;
-}
